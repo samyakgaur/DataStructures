6b1bf70708ab763b2d4ab4403c904e50b2830e3c
Samyak Gaur <samyakgaur@yahoo.com>
2019-08-22 14:36:33 +0530

    Add files via upload

--- linklist.c
+++ linklist.c
@@ -63,7 +63,7 @@ void insert_bef(int x,int val){
 		start=p;
 	else{
 		ptr=start;
-		while(ptr->info!=val){
+		while(ptr->info!=val && ptr!=NULL){
 			preptr=ptr;
 			ptr=ptr->next;
 		}
@@ -89,7 +89,7 @@ void insert_after(int x,int val){
 	}
 	
 }//End of insert_after function
-void delete(int val){
+void delete_specific(int val){
 	struct node*ptr,*previous;
 	if(start==NULL){
 		printf("The list is empty");
@@ -118,13 +118,113 @@ void delete(int val){
 	}//End of else condition	
 }//End of delete-specific function
 
+void delete_beg(){
+	struct node*ptr;
+	if(start==NULL){
+		printf("The list is empty");
+		return;
+	}//End of if function
+	else if (start->next==NULL){
+		free(start);
+		start=NULL;
+	}//if there's only one node in the list
+	else{
+	ptr=start;
+	start=ptr->next;
+	printf("Deleted node is: %d",ptr->info);
+	free(ptr);
+	}	
+}//End of delete_beg function
 
+void delete_last(){
+	struct node*ptr,*preptr;
+	if(start==NULL){
+		printf("The list is empty");
+		return;
+	}//End of if function
+	else if (start->next==NULL){
+		free(start);
+		start=NULL;
+	}//if there's only one node in the list
+	else{
+		ptr=start;
+		while(ptr->next!=NULL){
+				preptr=ptr;
+				ptr=ptr->next;
+			}
+		preptr->next=NULL;
+		printf("Deleted node is: %d",ptr->info);
+		free(ptr);
+	}	
+}//End of delete_last function
+
+
+void delete_bef(int val){
+	struct node*ptr,*preptr,*prepre;
+	if(start==NULL){
+		printf("The list is empty");
+		return;
+	}//End of if function
+	else if (start->next==NULL)
+		printf("The list has only one element please try other delete functions ! \n ");
+	else{
+		ptr=start;
+		while(ptr->info!=val){
+				prepre=preptr;
+				preptr=ptr;
+				ptr=ptr->next;
+			}
+		prepre->next=ptr;
+		printf("Deleted node is: %d",preptr->info);
+		free(preptr);
+	}	
+}//End of delete_bef function
+
+void delete_after(int val){
+	struct node*ptr,*nptr,*nnptr;
+	if(start==NULL){
+		printf("The list is empty");
+		return;
+	}//End of if function
+	else if (start->next==NULL)
+		printf("The list has only one element please try other delete functions ! \n ");
+	else{
+		ptr=start;
+		while(ptr->info!=val){
+				ptr=ptr->next;
+				nptr=ptr->next;
+				nnptr=nptr->next;	
+			}
+		ptr->next=nnptr;
+		printf("Deleted node is: %d",nptr->info);
+		free(nptr);
+	}	
+}//End of delete_after function
+
+void count(){
+	struct node *ptr;
+	int count=1;
+	if(start==NULL){
+			printf("The link list is empty ");
+			return;
+		}
+	else{
+		ptr=start;
+		while(ptr->next!=NULL){
+				ptr=ptr->next;
+				count+=1;
+			}
+	}//End of else condition
+	printf("The number of nodes in the list is: %d",count);
+}//End of count function
+
+ 
 int main(){
 	struct node *p,*previous,*ptr;
 	int choice,x,val;
 	p=(struct node *)malloc(sizeof(struct node));
 	while(1){
-		printf("\n Enter your choice: \n 1.Insert first \n 2.display \n 3.Add element at last \n 4.Insert a node after \n 5.Insert a node before \n 6. delete \n 7.Exit\n");
+		printf("\n Enter your choice: \n 1.Insert first \n 2.display \n 3.Add element at last \n 4.Insert a node after \n 5.Insert a node before \n 6. delete a specific node  \n 7. delete the first node \n 8. delete the last node \n 9. delete before a specific node \n 10. delete after a specific node\n 11. sort \n 12. count number of nodes \n 13.Exit\n");
 		scanf("%d",&choice);
 		switch(choice){
 			case 1:
@@ -158,9 +258,35 @@ int main(){
 			case 6: 
 					printf("Enter the value to delete: ");
 					scanf("%d",&x);
-					delete(x);
+					delete_specific(x);
 					break;
 			case 7: 
+					//delete beginning
+					delete_beg();
+					break; 
+			case 8: 
+					//delete end
+					delete_last();
+					break; 
+			case 9: 
+					//delete before specific
+					printf("Enter the value before which node should be deleted: ");
+					scanf("%d",&val);
+					delete_bef(val);
+					break; 
+			case 10: 
+					//delete after specific
+					printf("Enter the value after which node should be deleted: ");
+					scanf("%d",&val);
+					delete_after(val);
+					break;  
+			case 11: 
+					//sort
+			case 12: 
+					//Count number of nodes
+					count();
+					break;
+			case 13: 
 					exit(0);
 			default:
 					printf("Please enter a valid choice:");
